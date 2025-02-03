ccc5ff5751bc855d084b3940a1189e87779ba73f
soapy-ear <w1904477@my.westminster.ac.uk>
2025-02-04 05:44:50 +0900

    fix login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,7 +17,7 @@ const Login = ({ setAuth }) => {
     try {
       const body = { email, password };
       const response = await fetch(
-        `${process.env.REACT_APP_API_URL}/login`,
+        `${process.env.REACT_APP_API_URL}/auth/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
